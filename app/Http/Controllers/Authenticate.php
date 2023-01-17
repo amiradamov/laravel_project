fb18013c8cee78e04340870cb5bcd84fe98f2dc3
amiradamov <adamov.aamir@gmail.com>
2023-01-17 23:42:45 +0400

    Authenticate controller

--- app/Http/Controllers/Authenticate.php
+++ app/Http/Controllers/Authenticate.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Ingredient;
+use App\Models\Customer;
+use App\Models\Category;
+use Session;
+use Hash;
 
 class Authenticate extends Controller
 {
-    //
+    public function registrations() {
+
+    }
 }
