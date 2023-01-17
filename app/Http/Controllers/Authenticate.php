f739e90174842f7a5079baf315165925cc01ee2f
amiradamov <adamov.aamir@gmail.com>
2023-01-17 23:43:53 +0400

    Authenticate controller

--- app/Http/Controllers/Authenticate.php
+++ app/Http/Controllers/Authenticate.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 use App\Models\Ingredient;
 use App\Models\Customer;
 use App\Models\Category;
+use App\Models\Rating;
+use App\Models\Order;
+use App\Models\User;
+use App\Models\Item;
 use Session;
 use Hash;
 
